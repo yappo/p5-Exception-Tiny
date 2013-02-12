4cc54792bb277d746c47ab7d865a77c8a7c3537d
Kazuhiro Osawa <yappo@shibuya.pl>
2013-02-12 21:47:39 +0900

    initial import

---
+++ lib/Exception/Tiny.pm
@@ -0,0 +1,41 @@
+package Exception::Tiny;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Exception::Tiny - ...
+
+=head1 SYNOPSIS
+
+  use Exception::Tiny;
+
+=head1 DESCRIPTION
+
+Exception::Tiny is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {@} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kazuhiro Osawa
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
